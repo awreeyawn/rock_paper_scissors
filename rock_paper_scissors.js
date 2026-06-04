018af026a66f187250ee290a21822eb2e30fc21c
Arrian Morshed <27amorshed@cpsd.us>
2026-06-04 13:03:34 -0400

    moved pseudo code to js file as a block comment instead of in the html file (oops!)

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,6 +1,20 @@
+/*
+1. randomly pick a number 1-3 for rock, paper, or scissors
+2. set the computer's choice to be either rock paper or scissors based on the random number
+3. ask for user input for either rock, paper or scissors (ask user for actual choice, not 1-3)
+4. then compare the choices to determine the winner (paper beats rock, rock beats scissors, scissors beats paper)
+5. decide the winner of the game based what the computer and the user chose
+*/
+
+
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
+}
+
+function getHumanChoice() {
+    const userInput = prompt("Please enter rock, paper, or scissors:");
+    return userInput.toLowerCase();
 }
    
