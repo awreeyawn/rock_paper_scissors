b74b283a5e7041a48121b7613c56dd11a73571b9
Arrian Morshed <27amorshed@cpsd.us>
2026-06-04 13:26:22 -0400

    made all winner scenarios (boolean hell) and loss scenarios

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -27,6 +27,16 @@ function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         return "It's a tie!";
     }
+    else if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+             (humanChoice === 'paper' && computerChoice === 'rock') ||
+             (humanChoice === 'scissors' && computerChoice === 'paper')) {
+        humanScore++;
+        return "You win! " + humanChoice + " beats " + computerChoice;
+    }
+    else {
+        computerScore++;
+        return "You lose! " + computerChoice + " beats " + humanChoice;
+    }
 
 
 }
